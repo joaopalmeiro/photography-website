b99248437fb7f95c78ea3a720ae5043dd8cc8fc5
João Palmeiro <joaommpalmeiro@gmail.com>
2023-07-06 17:24:52 +0100

    Update photo page

--- pages/[collection]/[id].vue
+++ pages/[collection]/[id].vue
@@ -1,17 +1,27 @@
+<!-- https://gallery-nuxt.vercel.app/ -->
+
 <script setup lang="ts">
 const route = useRoute()
 const { collections } = useAppConfig()
 
-const currentCollection = collections.find(collection => collection.slug === route.params.collection)
+const currentCollection = collections.find(
+  collection => collection.slug === route.params.collection
+)
 const numberPhotos = currentCollection?.photos.length
 
-const pagination = computed(() => {
-  return `${route.params.id} of ${numberPhotos}`
-})
+const currentPhoto = currentCollection?.photos.find(
+  photo => photo.id.toString() === route.params.id
+)
+const currentPhotoSrc = currentPhoto?.src
+
+const pagination = `${route.params.id} of ${numberPhotos}`
 </script>
 
 <template>
-  <div>
-    {{ pagination }}
+  <div class="h-screen flex items-center justify-center">
+    <div class="flex flex-col items-center gap-4">
+      <NuxtImg :src="currentPhotoSrc" />
+      <p>{{ pagination }}</p>
+    </div>
   </div>
 </template>
