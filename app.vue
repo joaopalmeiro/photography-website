4c3a6696726c2a068b12094c953e57645f66052a
João Palmeiro <joaommpalmeiro@gmail.com>
2023-07-06 12:48:48 +0100

    Refactor

--- app.vue
+++ app.vue
@@ -1,15 +1,12 @@
 <!-- https://www.hyperui.dev/blog/how-to-write-better-containers-in-tailwindcss -->
+<!-- https://css-tricks.com/link-header-header-link/ -->
 
 <template>
   <div class="max-w-screen-xl mx-auto px-4">
     <ProfileHeader />
 
     <div class="grid grid-cols-6 gap-2">
-      <NuxtLink to="/p" class="cursor-zoom-in">
-        <div>
-          <h2>Black & Night</h2>
-        </div>
-      </NuxtLink>
+      <CollectionTitle title="Black & Night" />
       <CollectionCard />
       <CollectionCard />
       <CollectionCard />
---
+++ components/CollectionTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <NuxtLink to="/p" class="cursor-zoom-in">
+    <h2>{{ title }}</h2>
+  </NuxtLink>
+</template>
